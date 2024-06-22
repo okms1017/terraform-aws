e0ce9e1899a529a290eb1e0831d7b6a2de9d9d9d
okms1017 <okms1017@gmail.com>
2024-06-22 19:28:20 +0900

    Deploy a customizing web ec2

---
+++ examples/custom-web-ec2/sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "seong_ec2_sg" {
+  vpc_id      = aws_vpc.seong_vpc.id
+  name        = "seong-ec2-sg"
+  description = "seong-ec2-sg"
+}
+
+resource "aws_security_group_rule" "seong_sg_inbound" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.seong_ec2_sg.id
+}
+
+resource "aws_security_group_rule" "seong_sg_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.seong_ec2_sg.id
+}
+
+output "aws_security_group_id" {
+  value       = aws_security_group.seong_ec2_sg.id
+}
---
+++ examples/custom-web-ec2/vpc.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+resource "aws_vpc" "seong_vpc" {
+  cidr_block       = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "seong-vpc"
+  }
+}
+
+resource "aws_subnet" "seong_subnet1" {
+  vpc_id     = aws_vpc.seong_vpc.id
+  cidr_block = "10.10.1.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "seong-subnet1"
+  }
+}
+
+resource "aws_subnet" "seong_subnet2" {
+  vpc_id     = aws_vpc.seong_vpc.id
+  cidr_block = "10.10.2.0/24"
+
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "seong-subnet2"
+  }
+}
+
+resource "aws_internet_gateway" "seong_igw" {
+  vpc_id = aws_vpc.seong_vpc.id
+
+  tags = {
+    Name = "seong-igw"
+  }
+}
+
+resource "aws_route_table" "seong_rt" {
+  vpc_id = aws_vpc.seong_vpc.id
+
+  tags = {
+    Name = "seong-rt"
+  }
+}
+
+resource "aws_route_table_association" "seong_rt_association1" {
+  subnet_id      = aws_subnet.seong_subnet1.id
+  route_table_id = aws_route_table.seong_rt.id
+}
+
+resource "aws_route_table_association" "seong_rt_association2" {
+  subnet_id      = aws_subnet.seong_subnet2.id
+  route_table_id = aws_route_table.seong_rt.id
+}
+
+resource "aws_route" "seong_default_route" {
+  route_table_id         = aws_route_table.seong_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.seong_igw.id
+}
+
+
+output "aws_vpc_id" {
+  value = aws_vpc.seong_vpc.id
+}
